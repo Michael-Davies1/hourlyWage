cdd2b8d8cfa6f25b92317e231de7331ea145bfd0
Michael Davies <MD210515@reigate.ac.uk>
2021-09-20 10:16:42 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("please enter the emmployee name ");
+        String employeeName = input.next();
+        System.out.print("please enter the hours worked");
+        double hoursWorked = input.nextDouble();
+        System.out.println("please enter your hourly wage");
+        double hourlyWage = input.nextDouble()
+                if (hoursWorked > 45)  {
+                    double additionalHour = hoursWorked - 45;
+                    double overtimePay = 
+                }
+
+    }
+}
